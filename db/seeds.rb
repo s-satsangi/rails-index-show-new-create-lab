aaf1b7eec316c7c58a5c8133d6349c8f27ee5db6
s-satsangi <s.satsangi@gmail.com>
2020-10-28 16:17:37 -0400

    3 pm

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+15.times do 
+    Coupon.create(coupon_code: Faker::Code.npi, store: Faker::Restaurant.name)
+end
